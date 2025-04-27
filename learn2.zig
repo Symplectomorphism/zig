06862e085026e9187368fb383b1194f28b3bc6c7
Aykut C. Satici <acsatici@gmail.com>
2025-04-26 20:21:54 -0600

    Enums, Optional

---
+++ learn2.zig
@@ -0,0 +1,89 @@
+const std = @import("std");
+
+pub fn main() void {
+    const myStatus = Status.ok;
+    const myStage = Stage.confirmed;
+
+    std.debug.print("{any}\n", .{myStatus});
+    std.debug.print("{}\n\n", .{myStage.isComplete()});
+
+    const n = Number{.int = 32};
+    std.debug.print("{d}\n", .{n.int});
+
+    const ts = Timestamp{.unix = 1693278411};
+    std.debug.print("{d}\n", .{ts.seconds()});
+
+    const home: ?[]const u8 = null;
+    var name: ?[]const u8 = "Leto";
+
+    name = "Leto";
+
+    std.debug.print("{s}\n", .{name.?});
+
+    if (home) |h| {
+        // h is a []const u8
+        // we have a home value
+        std.debug.print("{s}\n", .{h.?});
+    } else {
+        // we don't have a home value
+        const char = "d";
+        std.debug.print("{c}\n", .{char});
+    }
+}
+
+// could be "pub"
+const Status = enum {
+    ok,
+    bad,
+    unknown,
+};
+
+const Stage = enum {
+    validate,
+    awaiting_confirmation,
+    confirmed,
+    err,
+
+    fn isComplete(self: Stage) bool {
+        return self == .confirmed or self == .err;
+    }
+};
+
+
+const Number = union {
+    int: i64,
+    float: f64,
+    nan: void,
+};
+
+
+const TimestampType = enum {
+    unix,
+    datetime,
+};
+
+const Timestamp = union(TimestampType) {
+    unix: i32,
+    datetime: DateTime,
+
+    const DateTime = struct {
+        year: u16,
+        month: u8, 
+        day: u8,
+        hour: u8,
+        minute: u8,
+        second: u8,
+    };
+
+    fn seconds(self: Timestamp) u16 {
+        switch (self) {
+            .datetime => |dt| return dt.second,
+            .unix => |ts| {
+                const seconds_since_midnight: i32 = @rem(ts, 86400);
+                return @intCast(@rem(seconds_since_midnight, 60));
+            },
+        }
+    }
+};
+
+
